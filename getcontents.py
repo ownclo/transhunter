441889628b561aefe9015373315d68223be0c08f
ownclo - gamma <mrownclo@gmail.com>
2012-11-08 02:44:18 +0400

    Dealt with advanced parsing and cyrillic chars

--- getcontents.py
+++ getcontents.py
@@ -1,8 +1,12 @@
+from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
 
-f = open ('test.html', 'r')
+f = urlopen('http://google.ru/')
 data = f.read()
-f.close()
 
-soup = BeautifulSoup (data)
-print (soup.prettify())
+soup = BeautifulSoup (data, fromEncoding="cp1251")
+string = soup.prettify()
+
+for item in soup.findAll(True):
+    if item.string and item.name == 'a':
+        print (item.string)
---
+++ habratopics.py
@@ -0,0 +1,8 @@
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup
+
+page = urlopen("http://habrahabr.ru/")
+soup = BeautifulSoup(page.read())
+
+for topic in soup.findAll(True, 'post_title'):
+    print ("%s -- %s" % (topic.contents[0], topic['href']))
