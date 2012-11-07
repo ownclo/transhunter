9a3d4235e53ef90763f6e63b1a4d8befc756c70f
ownclo - gamma <mrownclo@gmail.com>
2012-11-07 23:50:41 +0400

    Beautiful Soup module used instead. Cyrillic sucks

--- getcontents.py
+++ getcontents.py
@@ -1,11 +1,8 @@
-from urllib2 import urlopen
-from lxml.html import parse, open_in_browser, tostring
+from BeautifulSoup import BeautifulSoup
 
-testfile = open ('test.html', 'w')
-data = parse('http://google.com').getroot()
-string = tostring(data, pretty_print=True)
-testfile.write (string)
-testfile.close ()
-#for element in data.body.iter():
-#    print ("%s --- %s" % (element.tag, element.text))
-#print (datainfo.encoding)
+f = open ('test.html', 'r')
+data = f.read()
+f.close()
+
+soup = BeautifulSoup (data)
+print (soup.prettify())
