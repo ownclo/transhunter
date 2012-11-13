b69d94f0ee64474a2f8f434e347264a1e1501596
Ownclo - Arcis <mrownclo@gmail.com>
2012-11-13 23:57:36 +0400

    Got sore_epicness from comments

--- habarticle.py
+++ habarticle.py
@@ -3,6 +3,11 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
+def get_score(score_tag):
+    score_string = score_tag['title'].split(':')[0]
+    score_num = int (score_string.split()[1])
+    return score_num
+
 #page = urlopen("http://habrahabr.ru/post/158385/")
 with open ('article.html', 'r') as page:
     soup = BeautifulSoup(page.read())
@@ -11,5 +16,6 @@ id_pattern = re.compile("^comment_")
 adict = {'class':'comment_item', 'id':id_pattern}
 
 for comment in soup.findAll (name = 'div', attrs = adict):
-    score = comment.find (name = 'span', attrs = {'class' : 'score'})
-    print (score.string)
+    score_tag = comment.find (name = 'span', attrs = {'class' : 'score'})
+    score = get_score(score_tag)
+    print (score)
