c9b694f1d0f00942d653fe9fe80c94f1ee1d7faa
Ownclo - Arcis <mrownclo@gmail.com>
2012-11-14 01:29:48 +0400

    Feature: pulling out sorted comments

--- habarticle.py
+++ habarticle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from operator import itemgetter
 import re
 
 def get_score(score_tag):
@@ -15,7 +16,15 @@ with open ('article.html', 'r') as page:
 id_pattern = re.compile("^comment_")
 adict = {'class':'comment_item', 'id':id_pattern}
 
+rated_comments = []
+
 for comment in soup.findAll (name = 'div', attrs = adict):
     score_tag = comment.find (name = 'span', attrs = {'class' : 'score'})
     score = get_score(score_tag)
-    print (score)
+    rated_comments.append({'comment' : comment, 'score' : score})
+
+sorted_comments = sorted (rated_comments, reverse = True, key=itemgetter('score'))
+for item in sorted_comments:
+    print (item['score'])
+#Haskell style:
+#list (map (lambda x: print (x['score']), sorted_comments))
